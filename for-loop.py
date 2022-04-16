19fc6b8dd95afc4121b2517acf8625f323348088
mayur4279 <73772313+mayur4279@users.noreply.github.com>
2022-04-16 11:58:30 +0530

    Update for-loop.py

--- for-loop.py
+++ for-loop.py
@@ -1,10 +1,23 @@
+#program to print 1 to 10 numbers 
+
 print("this is for loop")
  
-for i in range(1,4):
+for i in range(1,10):
 	print(i)
 newline() 
 
 
+
+#program to print numbers on list 
+
+numbers=[6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+for i in  numbers:
+	print (i)
+
+
+	
+	
 #program to find sum of all numbers
 
 numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
@@ -15,5 +28,21 @@ for i in numbers:
 	a=a+i
 
 print("the addition of numbers is", a )
+
+
+
+# program to display student's marks from record 
+
+student_name=str(input("enter a student name="))
+
+marks={'mayur': 90, 'Jules': 55, 'Arthur': 77} 
+
+for student in marks:
+	if student == student_name:
+	  print(marks[student])
+	  break
+else:
+    print("no entry found in dataset.")
+
 
 
