bbcd36b86cbb46e2d5fedb110aba0adea16d3f45
mayur4279 <73772313+mayur4279@users.noreply.github.com>
2022-04-16 11:39:00 +0530

    Create for-loop.py

---
+++ for-loop.py
@@ -0,0 +1,19 @@
+print("this is for loop")
+ 
+for i in range(1,4):
+	print(i)
+newline() 
+
+
+#program to find sum of all number 
+
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+a=0
+
+for i in numbers:
+	a=a+i
+
+print("the addition of numbers is", a )
+
+
